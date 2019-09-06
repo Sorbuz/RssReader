563596b6fa7e8381fe16db100926480f25c6596f
Albin Wejros <albin.wejros@gmail.com>
2019-09-06 16:43:45 +0200

    Adding GET methods for feeds

--- RssReader.API/Controllers/RssFeedsController.cs
+++ RssReader.API/Controllers/RssFeedsController.cs
@@ -1,24 +1,34 @@
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RssReader.API.Data;
 
 namespace RssReader.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RssFeedsController
+  [Route("api/[controller]")]
+  [ApiController]
+  public class RssFeedsController : ControllerBase
+  {
+    private readonly DataContext context;
+    public RssFeedsController(DataContext context)
     {
-         // GET api/rssfeeds
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value12", "value2" };
-        }
-
-        // GET api/rssfeeds/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
+      this.context = context;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetRssFeeds()
+    {
+      var rssFeeds = await context.RssFeeds.ToListAsync();
+
+      return Ok(rssFeeds);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetRssFeed(int id)
+    {
+      var rssFeed = await context.RssFeeds.FirstOrDefaultAsync(x => x.Id == id);
+
+      return Ok(rssFeed);
+    }
+  }
 }
